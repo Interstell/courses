7f5c4bd983a254b9123f8451cf5fbe0ddcff41f8
Interstell <urukovdima@gmail.com>
2015-10-02 18:57:26 +0300

    done

---
+++ courses/prog_base/labs/lab1/math.c
@@ -0,0 +1,18 @@
+#include <math.h>
+
+double calc(double x, double y, double z) {
+	double a;
+	double a0, a1, a2;
+	double n0 = 7, n1 = 1;
+	a0 = pow(x, y + 1) / pow(x - y, 1 / z);
+	a1 = y / ((n0 + 1)*abs(x + y));
+	a2 = sqrt(abs(cos(y) / sin(x) + n1));
+	a = a0 + a1 + a2;
+	if (isfinite(a)){
+		return a;
+	}
+	else {
+		return nan("") ;
+	}
+}
+	
