5b93df9b16b3931f9bba6f098f10b2ad705f5fb2
Interstell <urukovdima@gmail.com>
2015-10-03 17:19:33 +0300

    done

--- courses/prog_base/labs/lab1/condition.c
+++ courses/prog_base/labs/lab1/condition.c
@@ -2,11 +2,9 @@
 
 int isExp2(int modmin){
 	double b = (log(modmin) / log(2)) - (int)(log(modmin) / log(2));
-	if (b==0){
-		return 1;
-	}
-	else return 0;
+	return (!b) ? 1 : 0;
 }
+
 int NumOfNeg(int a, int b, int c){
 	int result = 0;
 	if (a < 0) result++;
@@ -21,8 +19,8 @@ int satisfies(int a, int b, int c) {
 	int modmin, sum2;
 
 	if (NumOfNeg(a,b,c)==3){
-		modmin = abs(min(a, b, c));
-		sum2 = a + b + c - min(a, b, c);
+		modmin = abs(min(min(a, b), min(b, c)));
+		sum2 = a + b + c - min(min(a, b), min(b, c));
 		if ((sum2 < -256) && (isExp2(modmin) == 1) && (modmin < 256)){
 			return 1;
 		}
@@ -33,23 +31,14 @@ int satisfies(int a, int b, int c) {
 	}
 	else if ((NumOfNeg(a,b,c)==1)||(NumOfNeg(a,b,c)==2)){ 
 		if (NumOfNeg(a, b, c) == 1){
-			if (min(a, b, c, )>-256){
-				return 1;
-			}
-			else return 0;
+			return (min(min(a, b), min(b, c))>-256) ? 1 : 0;
 		}
 		if (NumOfNeg(a, b, c) == 2){
-			if ((a + b + c - max(a, b, c))*(n0 + n1) > -256){
-				return 1;
-			}
-			else return 0;
+			return ((a + b + c - max(max(a, b), max(b, c)))*(n0 + n1) > -256) ? 1 : 0;
 		}
 	}
 	else if (NumOfNeg(a,b,c)==0) {  
-		if (max(a, b, c)*min(a, b, c) < 256){
-			return 1;
-		}
-		else return 0;
+		return (max(max(a, b), max(b, c))*max(max(a, b), max(b, c)) < 256) ? 1 : 0;
 	}
 	return result;
 }
