030c1c8aed9c59afda025cae3459236d68c8b0ff
Interstell <urukovdima@gmail.com>
2015-11-13 22:24:36 +0200

    redone

--- courses/prog_base/tasks/console_graph/main.c
+++ courses/prog_base/tasks/console_graph/main.c
@@ -18,7 +18,7 @@ COORD getNewCentre(COORD centre, int x, int y){
 	centre.Y -= y;
 	return centre;
 }
-void printAxes(COORD centre, int shiftX, int shiftY){
+void printAxes(COORD centre, int shiftX, int shiftY, int zoom){
 	COORD pos;
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
@@ -26,16 +26,28 @@ void printAxes(COORD centre, int shiftX, int shiftY){
 	if ((pos.Y >= 0) && (pos.Y <= 24)){
 		for (pos.X = 79; pos.X >= 0; pos.X--){
 			SetConsoleCursorPosition(hConsole, pos);
-			if ((pos.X-shiftX) % 5 == 0) printf("+");
-			else printf("-");
+			if (zoom <= 10){
+				if ((pos.X - shiftX + 2) % (6 - (int)((zoom - 10)*0.1)) == 0) printf("+");
+				else printf("-");
+			}
+			else {
+				if ((pos.X - shiftX + 2) % (6 - (int)((zoom - 10)*0.1)) == 0) printf("+");
+				else printf("-");
+			}
 		}
 	}
 	pos.X = centre.X;
 	if ((pos.X >= 0) && (pos.X <= 79)){
 		for (pos.Y = 24; pos.Y >= 0; pos.Y--){
 			SetConsoleCursorPosition(hConsole, pos);
-			if ((pos.Y+shiftY) % 4 == 0) printf("+");
-			else printf("|");
+			if (zoom <= 10){
+				if ((pos.Y - shiftY + 3) % (5 - (int)((zoom - 10)*0.1)) == 0) printf("+");
+				else printf("|");
+			}
+			else {
+				if ((pos.Y - shiftY + 3) % (5 + (int)((zoom - 10)*0.1)) == 0) printf("+");
+				else printf("|");
+			}
 		}
 	}
 }
@@ -55,7 +67,7 @@ COORD printConnector(COORD prevDot, COORD newDot){
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, BACKGROUND_BLUE | BACKGROUND_INTENSITY);
 	if (abs(newDot.Y - prevDot.Y) > 1){
-		if (newDot.Y > prevDot.Y){
+		if (newDot.Y > prevDot.Y){ //вниз
 			prevDot.X += newDot.X - prevDot.X;
 			while (prevDot.Y != newDot.Y){
 				if ((prevDot.X >= 0) && (prevDot.X <= 79) && (prevDot.Y >= 0) && (prevDot.Y <= 24)){
@@ -66,7 +78,6 @@ COORD printConnector(COORD prevDot, COORD newDot){
 			}
 		}
 		else if (newDot.Y < prevDot.Y){
-			prevDot.Y = (newDot.Y - prevDot.Y < 0) ? (prevDot.Y - 1) : (prevDot.Y + 1);
 			while (prevDot.Y != newDot.Y){
 				if ((prevDot.X >= 0) && (prevDot.X <= 79) && (prevDot.Y >= 0) && (prevDot.Y <= 24)){
 					SetConsoleCursorPosition(hConsole, prevDot);
@@ -81,16 +92,14 @@ COORD printConnector(COORD prevDot, COORD newDot){
 		}
 	}
 }
-int func2(int x, int zoomX, int zoomY){
-	zoomY+=10;
-	if (zoomY < 0) zoomY = 0;
-	return (pow(zoomX*x*0.1*0.5,2) - 3)*(0.05*zoomY);
+int func2(int x, int zoom){
+	if (zoom < 0) zoom = 0;
+	return (pow(zoom*x*0.1*0.5,2) - 3)*(0.05*zoom)*(-1)*(2);
 }
-int func1(int x, int zoomX, int zoomY){
-	zoomX = zoomX + (zoomX - 10)*0.01;
-	return (tan(pow(zoomX*x*0.1, 2)) + pow(sin(2 *x*zoomX*0.1), 2))*(0.5*0.1*zoomY);
+int func1(int x, int zoom){
+	return (tan(pow(zoom*x*0.1, 2)) + pow(sin(2 *x*zoom*0.1), 2))*(0.5*0.1*zoom);
 }
-void printLegend(void){
+void printLegend(funcNum){
 	COORD legend;
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	legend.X = 1;
@@ -101,21 +110,27 @@ void printLegend(void){
 		printf(" ");
 	}
 	SetConsoleCursorPosition(hConsole, legend);
-	printf(" Switch plot: ENTER|Axis move: WASD|ZOOM: Ctrl+WASD|Restart: SPACE|Exit: ESC");
+	printf("Switch plot: ENTER | Axis move: WASD | ZOOM: + - | Restart: SPACE | Exit: ESC");
+	legend.X = 0;
+	legend.Y = 23;
+	SetConsoleCursorPosition(hConsole, legend);
+	SetConsoleTextAttribute(hConsole, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+	if (funcNum == 1) printf(" F(x)=tg(x^2)+sin(2*x)^2");
+	else printf(" F(x)=-x^2+3");
 }
-void refreshScreen(COORD centre, int zoomX, int zoomY, int shiftX, int shiftY, int funcNum){
+void refreshScreen(COORD centre, int zoom, int shiftX, int shiftY, int funcNum){
 	COORD prevDot, newDot, legend;
 	fillBg();
-	printAxes(centre, shiftX, shiftY);
-	if (funcNum == 1) prevDot = printDot(centre, -60, func1(-60, zoomX, zoomY));
-	else prevDot = printDot(centre, -60, func2(-60, zoomX,zoomY));
+	printAxes(centre, shiftX, shiftY, zoom);
+	if (funcNum == 1) prevDot = printDot(centre, -60, func1(-60, zoom));
+	else prevDot = printDot(centre, -60, func2(-60, zoom));
 	for (int i = -59; i <= 60; i++){
-		if (funcNum==1) newDot = printDot(centre, i, func1(i, zoomX, zoomY));
-		else newDot = printDot(centre, i, func2(i, zoomX, zoomY));
+		if (funcNum==1) newDot = printDot(centre, i, func1(i, zoom));
+		else newDot = printDot(centre, i, func2(i, zoom));
 		printConnector(prevDot, newDot);
 		prevDot = newDot;
 	}
-	printLegend();
+	printLegend(funcNum);
 }
 void printIntro(void){
 	COORD pos;
@@ -146,62 +161,53 @@ void printIntro(void){
 }
 int main(void){
 	COORD centre = { 40, 12 };
-	int btnInput, shiftX=0, shiftY=0, funcNum=1, zoomX=10, zoomY=10;
+	int btnInput, shiftX=0, shiftY=0, funcNum=1, zoom=10;
 	printIntro();
 	getch();
-	refreshScreen(centre, zoomX, zoomY, 0, 0, funcNum);
+	refreshScreen(centre, zoom, 0, 0, funcNum);
 	while (1){
 		btnInput = getch();
 		if (btnInput == 27) break;
 		switch (btnInput) {
 		case 32:
-			zoomX = 10;
-			zoomY = 10;
+			zoom = 10;
 			shiftX = 0;
 			shiftY = 0;
 			centre.X = 40;
 			centre.Y = 12;
-			refreshScreen(centre, zoomX, zoomY, 0, 0, funcNum);
+			refreshScreen(centre, zoom, 0, 0, funcNum);
 			break;
 		case 13:
 			funcNum = (funcNum == 1) ? 2 : 1;
-			refreshScreen(centre, zoomX, zoomY, 0, 0, funcNum);
+			refreshScreen(centre, zoom, 0, 0, funcNum);
 			break;
 		case 230: // W - up
 			centre = getNewCentre(centre, 0, 1);
 			shiftY++;
-			refreshScreen(centre, zoomX, zoomY, shiftX, shiftY, funcNum);
+			refreshScreen(centre, zoom, shiftX, shiftY, funcNum);
 			break;
 		case 235: // S - down
 			centre = getNewCentre(centre, 0, -1);
 			shiftY--;
-			refreshScreen(centre, zoomX, zoomY, shiftX, shiftY, funcNum);
+			refreshScreen(centre, zoom, shiftX, shiftY, funcNum);
 			break;
 		case 162: // D - right
 			centre = getNewCentre(centre, 1, 0);
 			shiftX++;
-			refreshScreen(centre, zoomX, zoomY, shiftX, shiftY, funcNum);
+			refreshScreen(centre, zoom, shiftX, shiftY, funcNum);
 			break;
 		case 228: // A - left
 			centre = getNewCentre(centre, -1, 0);
 			shiftX--;
-			refreshScreen(centre, zoomX, zoomY, shiftX, shiftY, funcNum);
+			refreshScreen(centre, zoom, shiftX, shiftY, funcNum);
 			break;
-		case 23: //Zoom Y+
-			zoomY++;
-			refreshScreen(centre, zoomX, zoomY, shiftX, shiftY, funcNum);
+		case 43: //Zoom +
+			zoom--;
+			refreshScreen(centre, zoom, shiftX, shiftY, funcNum);
 			break;
-		case 19: //Zoom Y-
-			zoomY--;
-			refreshScreen(centre, zoomX, zoomY, shiftX, shiftY, funcNum);
-			break;
-		case 4: //Zoom X++
-			zoomX++;
-			refreshScreen(centre, zoomX, zoomY, shiftX, shiftY, funcNum);
-			break;
-		case 1: //Zoom X--
-			zoomX--;
-			refreshScreen(centre, zoomX, zoomY, shiftX, shiftY, funcNum);
+		case 45: //Zoom -
+			zoom++;
+			refreshScreen(centre, zoom, shiftX, shiftY, funcNum);
 			break;
 		}
 	}
